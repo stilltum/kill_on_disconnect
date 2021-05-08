1c79b2ad1e079d69befddd4f8459f12265238785
stilltum <35137975+stilltum@users.noreply.github.com>
2021-05-08 23:32:57 +0800

    Delete kill_on_disconnect.sh

--- kill_on_disconnect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#if ifconfig en0 inet | grep "inet" ;
-if /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep "wifi" ;
-then
-	echo "network up"
-else
-	echo "network down"
-	pkill -f Firefox
-fi
