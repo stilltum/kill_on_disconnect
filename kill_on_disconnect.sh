694d7a57d0629ccbcd3e905f2c00933d40ff6887
stilltum <35137975+stilltum@users.noreply.github.com>
2021-05-08 23:32:43 +0800

    Update kill_on_disconnect.sh

--- kill_on_disconnect.sh
+++ kill_on_disconnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #if ifconfig en0 inet | grep "inet" ;
-if /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep "OurHouse_5G" ;
+if /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep "wifi" ;
 then
 	echo "network up"
 else
